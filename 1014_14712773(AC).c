804b592878e54b7073d06be483436f0620e7f812
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-08-22 21:25:05 +0900

    Add files via upload

---
+++ 1014_14712773(AC).c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char x, y;
+    scanf("%c %c", &x, &y);
+    printf("%c %c", y, x); 
+    return 0;
+}
---
+++ 1028_14713205(AC).c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    unsigned int n;
+    scanf("%u", &n);
+    printf("%u", n);
+
+    
+    return 0;
+}
