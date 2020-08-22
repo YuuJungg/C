5a44ff2cbf3e4ecafde79c234d881698de92cdcb
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-08-22 20:58:25 +0900

    Add files via upload

---
+++ 1022_14712975(AC).c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char data[2001];
+    // fgets()를 사용하면 공백문자가 포함되어있는 문장 입력받아 저장할 수 있다.
+    fgets(data, 2000, stdin);
+    printf("%s", data);
+
+    return 0;
+}
