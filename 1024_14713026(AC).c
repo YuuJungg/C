439efb6b0b8c23df55842f11d3f218a700ad00ba
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-08-22 21:03:34 +0900

    Add files via upload

---
+++ 1024_14713026(AC).c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char d[30]; 
+    scanf("%s", d); 
+    for(int i=0; d[i]!='\0'; i++) //저장된 내용을 하나하나씩 검사해서 널문자가 아닐 동안 아래 실행
+    {
+        printf("\'%c\'\n", d[i]);
+    }   
+
+    return 0;
+}
