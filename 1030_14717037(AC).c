99d4f9e0cb12744db05f58fd5c5a3b23a591edb1
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-08-23 19:04:35 +0900

    Add files via upload

---
+++ 1030_14717037(AC).c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+
+
+    return 0;
+}
---
+++ 1036_14717088(AC).c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char x;
+    scanf("%c", &x);
+    printf("%d", x);
+
+
+    return 0;
+}
