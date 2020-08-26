0db0db6ba109ddfe15fc126a5f213049a7ae9e48
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-08-26 14:39:42 +0900

    Add files via upload

---
+++ 1067_14753560(AC).c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    if(a>0)
+        printf("plus\n");
+    else
+        printf("minus\n");
+        
+    if(a%2==0)
+        printf("%s\n", "even");    
+    else
+        printf("%s\n", "odd");
+
+    
+    
+    
+    return 0;
+}
---
+++ 1069_14753691(AC).c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c", &a);
+
+    switch(a)
+    {
+    case 'A': 
+        printf("best!!!\n");
+        break;
+    case 'B':
+        printf("good!!\n");
+        break;
+    case 'C':
+        printf("run!\n");
+        break;
+    case 'D':
+        printf("slowly~\n");
+        break;
+    default:
+        printf("what?\n");
+    }
+    
+    
+    
+    
+    return 0;
+}
